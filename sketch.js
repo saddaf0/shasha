11459edb65393b5bf7cb265cf21459831f8c1239
saddaf0 <78302873+saddaf0@users.noreply.github.com>
2023-03-04 22:30:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ var ball
 var gameState = PLAY
 var PLAY = 1
 var END = 0
-
+var obstaclesGroup
 
 
 
@@ -17,8 +17,8 @@ function preload(){
 bellaImg = loadImage("Dog 3.png")
 sceneImg = loadImage("scene garden.png")
 bellaRun = loadAnimation("Dog 3.png" , "Dog 2.png" , "Dog 1.png");
-ball = loadImage("ball.png")
-bone = loadImage("bone.png")
+obstacle1 = loadImage("ball.png")
+obstacle2= loadImage("bone.png")
 
 }
 
@@ -33,6 +33,8 @@ scene.velocityX=-5
 bella = createSprite(width/11,height-80,20,20)
 bella.addAnimation("running" , bellaRun)
 bella.scale = 1.2
+
+obstaclesGroup=new Group()
 
 score=0
 
@@ -49,21 +51,38 @@ function draw() {
       }
     
 
-  
+  spawnObstacles()
 
 drawSprites();
 
 text("Score: "+ score, width/11,height/14);
   
 
-  
-
-
-
-
-
-
 
 
 }
 
+function spawnObstacles(){
+  if (frameCount % 60 === 0){
+    var obstacle = createSprite(width,height-100,10,40);
+    obstacle.velocityX = -6;
+    
+     //generate random obstacles
+     var rand = Math.round(random(1,2));
+     switch(rand) {
+       case 1: obstacle.addImage(obstacle1);
+               break;
+       case 2: obstacle.addImage(obstacle2);
+               break;
+      
+       default: break;
+     }
+    
+     //assign scale and lifetime to the obstacle           
+     obstacle.scale = 0.2;
+     obstacle.lifetime = 300;
+    
+    //add each obstacle to the group
+     obstaclesGroup.add(obstacle);
+  }
+ }
